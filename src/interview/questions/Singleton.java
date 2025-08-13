89a5c14d9aadda4f2d5ab9aa9217ccc85ec4a6ed
Manikant Sangam <manikantsangam.7@gmail.com>
2025-08-13 08:23:25 +0530

    Added security from Serialization and Reflections

--- src/interview/questions/Singleton.java
+++ src/interview/questions/Singleton.java
@@ -1,29 +1,42 @@
 package interview.questions;
 
-public class Singleton implements Cloneable {
-	
-	private static volatile Singleton instance=null;
+import java.io.Serializable;
+
+public class Singleton implements Cloneable, Serializable {
+
+	// to ensure version compatibility during the serialization and deserialization of objects. 
+	private static final long serialVersionUID = 1L;
+	private static volatile Singleton instance = null;
 	private String data;
-	
+
+	// Private constructor with reflection defense
 	private Singleton(String data) {
-		this.data=data;
+		if (instance != null) {
+			throw new RuntimeException("Do not try to break the Singleton class");
+		}
+		this.data = data;
 	}
-	
+
 	public static Singleton getInstance(String data) {
-		if(instance==null) {
+		if (instance == null) {
 			synchronized (Singleton.class) {
-				if(instance==null) {
-					instance=new Singleton(data);
+				if (instance == null) {
+					instance = new Singleton(data);
 				}
 			}
 		}
 		return instance;
 	}
-	
-	 @Override
-	    protected Object clone() throws CloneNotSupportedException {
-	        // Prevent cloning by throwing an exception
-	        throw new CloneNotSupportedException("Singleton instance cannot be cloned.");
-	    }
+
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		// Prevent cloning by throwing an exception
+		throw new CloneNotSupportedException("Singleton instance cannot be cloned.");
+	}
+
+	// Method to prevent multiple instances during deserialization
+	protected Object readResolve() {
+		return getInstance(data);
+	}
 
 }
