6a5a3b5cbd30d311dee781f6a56d8f43b3eca322
Manikant Sangam <manikantsangam.7@gmail.com>
2025-08-16 01:25:04 +0530

    Removed unused import

--- src/stream/api/Question4.java
+++ src/stream/api/Question4.java
@@ -2,7 +2,6 @@ package stream.api;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
